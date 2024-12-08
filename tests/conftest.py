cbf3229952cd12ba9b58bc003562220fb871f7f4
Aleksey Stolbov <alstofly@gmail.com>
2024-12-08 13:39:08 +0300

    Контрольный коммит

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
-from typing import List, Tuple, Union
+from typing import Dict, List, Tuple, Union
 
 import pytest
 
 
 @pytest.fixture
-def transactions_fixture() -> List[dict]:
+def transactions_fixture() -> List[Dict[str, Union[int, str, Dict[str, str]]]]:
     """Тестовые данные с транзакциями"""
     return [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -71,4 +71,37 @@ def get_date_test_data() -> List[Tuple[str, Union[str, type]]]:
         ("", ValueError),
         ("2023-13-01", ValueError),
         ("Некорректные данные", ValueError),
+    ]
+
+
+@pytest.fixture
+def generators_transactions_fixture() -> List[Dict[str, Union[int, str, Dict[str, Union[str, Dict[str, str]]]]]]:
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 123456789,
+            "state": "PENDING",
+            "date": "2020-01-01T00:00:00.000000",
+            "operationAmount": {"amount": "1000", "currency": {"name": "EUR", "code": "EUR"}},
+            "description": "Перевод с карты на карту",
+            "from": "Счет 123456789",
+            "to": "Счет 987654321"
+        }
     ]
